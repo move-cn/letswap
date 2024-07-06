58f6fe28643081051a49b719d339e5121ca73ae9
uvd <386180127@qq.com>
2024-07-06 21:10:15 +0800

    task2

--- src/02_coin/code/coin_test/test.sh
+++ src/02_coin/code/coin_test/test.sh
@@ -5,3 +5,10 @@
 
 
  # c: &mut TreasuryCap<T>, amount: u64, recipient: address
+
+
+
+  sui client call --package 0x2 --module coin --function mint_and_transfer \
+  --args 0x7be885747efb3896505dac68963255e771c95c024026e59cef67fc280d369383 \
+  100000000  0x1fdcbc218b29c5d91eb445781016a24658c45825a8469b76f0efb289ccc89f86 \
+ --type-args "0xf7dd552ddf475902751428cde73de7bbeedb1bfd8ccd1cea392ff310a31c181e::share_coin::SHARE_COIN"
